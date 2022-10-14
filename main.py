49631c4ca8898c231edc4bcc9609e8c7557212ef
Ranadeep Singh <ranadees@andrew.cmu.edu>
2022-10-14 06:05:50 -0400

    Added test_model every 1 hour

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import time
 
 def hello_world():
     '''
@@ -12,3 +13,22 @@ def recommend(num_movies):
     '''
     movies_df = pd.read_csv('movies.csv', delimiter='|') # | as delimiter as movies usually have commas in their names
     return ','.join(movies_df.sample(num_movies).values[:,0].tolist()) # Sample 5 movies
+
+def train_model():
+    '''
+    Simulate Model Training and return training time in seconds
+    '''
+    start_time = time.time()
+    print("Training model...")
+    time.sleep(5)
+    print("Model trained!")
+
+    end_time = time.time()
+
+    return end_time - start_time
+
+if __name__ == '__main__':
+
+    print(hello_world())
+    train_model()
+    print(recommend(5))
